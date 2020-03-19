b342b889a3847c0f92c2814f6c93d9f5cfced9b2
Strackninja <strahinja.velickovic018@gmail.com>
2020-03-19 16:25:55 +0100

    03-19
    03-19

---
+++ Slutprojekt/Slutprojekt/Ideas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slutprojekt
+{
+    class Ideas
+    {
+        /* Ideas
+         * Musikspel,typ create your own orchestra grej, välj olika instrument (Piano, fiol, viola, cello, olika bläckblåsinstrument m.m som sedan ska spela i olika skalor )
+         * Fortsätta på mitt novemberprojekt, kanske försöka hitta en api som har faktiska stats. Eventuellt göra en "Create your own character" med olika atributer och stats som sedan ska möta datorn. 
+         * 
+         * 
+        */
+    }
+}
